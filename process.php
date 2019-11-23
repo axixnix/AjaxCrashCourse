a64b07b4335204b6a4a6d9f2713c1bf7f11e451c
B <henryirons@yahoo.com>
2019-11-23 23:56:55 +0100

    regular post form

--- process.php
+++ process.php
@@ -2,8 +2,8 @@
 echo 'processing...';
 
 //check for POST variable
-if(isset($_GET['name'])){
-    echo 'GET: your name is' . $_GET['name'];
+if(isset($_POST['name'])){
+    echo 'POST: your name is' . $_GET['name'];
 }
 
 //check for get variable
